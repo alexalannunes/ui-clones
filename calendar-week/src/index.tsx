9b8be64077cab0a3153d353fe06b5688aef9eeeb
Alex Alan Nunes <alexalannunes@gmail.com>
2022-07-29 18:14:35 -0300

    remove vars

--- calendar-week/src/index.tsx
+++ calendar-week/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
-import "./styles/theme.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "./theme/ThemeProvider";
