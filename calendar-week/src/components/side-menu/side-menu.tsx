0b5b0943592be63291d47e4629e81a11289ecc97
Alex Alan Nunes <alexalannunes@gmail.com>
2022-07-30 11:00:53 -0300

    make menu aside

--- calendar-week/src/components/side-menu/side-menu.tsx
+++ calendar-week/src/components/side-menu/side-menu.tsx
@@ -2,7 +2,26 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 const SideMenu: React.FC = () => {
-  return <div className={styles.container}>menu</div>;
+  return (
+    <aside className={styles.container}>
+      <div role="button" tabIndex={0} className={styles.menuItem}>
+        <span className="material-symbols-rounded">menu</span>
+      </div>
+      <div
+        role="button"
+        tabIndex={0}
+        className={`${styles.menuItem} ${styles.fillPrimary} ${styles.menuItemIcon}`}
+      >
+        MK
+      </div>
+      <div role="button" tabIndex={0} className={styles.menuItem}>
+        <span className="material-symbols-rounded">menu</span>
+      </div>
+      <div role="button" tabIndex={0} className={styles.menuItem}>
+        <span className="material-symbols-rounded">add</span>
+      </div>
+    </aside>
+  );
 };
 
 export { SideMenu };
