8818401d9d62319c58b406de48172bf4e392dbc3
Alex Alan Nunes <alexalannunes@gmail.com>
2022-08-01 12:33:27 -0300

    side-menu OK

--- calendar-week/src/components/side-menu/side-menu.tsx
+++ calendar-week/src/components/side-menu/side-menu.tsx
@@ -4,21 +4,40 @@ import styles from "./styles.module.scss";
 const SideMenu: React.FC = () => {
   return (
     <aside className={styles.container}>
-      <div role="button" tabIndex={0} className={styles.menuItem}>
-        <span className="material-symbols-rounded">menu</span>
+      <div className={styles.sideMenuTop}>
+        <div role="button" tabIndex={0} className={styles.menuItem}>
+          <span className="material-symbols-rounded">menu</span>
+        </div>
+        <div
+          role="button"
+          tabIndex={0}
+          className={`${styles.menuItem} ${styles.menuItemIcon} ${styles.fillPrimary}`}
+        >
+          MK
+        </div>
+        <div
+          role="button"
+          tabIndex={0}
+          className={`${styles.menuItem} ${styles.menuItemIcon} ${styles.fillOrange}`}
+        >
+          <span className="material-symbols-rounded">bolt</span>
+        </div>
+        <div
+          role="button"
+          tabIndex={0}
+          className={`${styles.menuItem} ${styles.menuItemIcon} ${styles.fillGray} ${styles.customSize}`}
+        >
+          <span className="material-symbols-rounded">add</span>
+        </div>
       </div>
-      <div
-        role="button"
-        tabIndex={0}
-        className={`${styles.menuItem} ${styles.fillPrimary} ${styles.menuItemIcon}`}
-      >
-        MK
-      </div>
-      <div role="button" tabIndex={0} className={styles.menuItem}>
-        <span className="material-symbols-rounded">menu</span>
-      </div>
-      <div role="button" tabIndex={0} className={styles.menuItem}>
-        <span className="material-symbols-rounded">add</span>
+      <div>
+        <div
+          role="button"
+          tabIndex={0}
+          className={`${styles.menuItem} ${styles.itemSettings}`}
+        >
+          <span className="material-symbols-rounded">settings</span>
+        </div>
       </div>
     </aside>
   );
